79597c90912ca96793b99896a10c1b5f52fa9f27
Diego Fincatto <diego@t3w.io>
2026-01-22 19:52:41 -0300

    Adicionado tipo 3

--- src/main/java/io/github/t3wv/nfse/municipal/nfseSPBarueri/classes/NFSeBarueriRPSArquivoEnvioRegistroTipo3.java
+++ src/main/java/io/github/t3wv/nfse/municipal/nfseSPBarueri/classes/NFSeBarueriRPSArquivoEnvioRegistroTipo3.java
@@ -1,43 +1,52 @@
 package io.github.t3wv.nfse.municipal.nfseSPBarueri.classes;
 
+import io.github.t3wv.nfse.municipal.nfseSPBarueri.enums.NFSeBarueriOutrosValoresTipo;
 import org.apache.commons.lang3.StringUtils;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class NFSeBarueriRPSArquivoEnvioRegistroTipo3 extends NFSeBarueriRPSArquivoEnvioRegistro<NFSeBarueriRPSArquivoEnvioRegistroTipo3> {
+
     static final String TIPO_REGISTRO = "3";
-//    private final NFSeBarueriRPSOutrosValores outrosValores;
-//
-//
-//    public NFSeBarueriRPSArquivoEnvioRegistroTipo3(NFSeBarueriRPSOutrosValores outrosValores) {
-//        this.outrosValores = outrosValores;
-//    }
+    private NFSeBarueriOutrosValoresTipo outrosValoresTipo;
+    private BigDecimal valor;
+
+    public NFSeBarueriOutrosValoresTipo getOutrosValoresTipo() {
+        return outrosValoresTipo;
+    }
+
+    public NFSeBarueriRPSArquivoEnvioRegistroTipo3 setOutrosValoresTipo(NFSeBarueriOutrosValoresTipo outrosValoresTipo) {
+        this.outrosValoresTipo = outrosValoresTipo;
+        return this;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public NFSeBarueriRPSArquivoEnvioRegistroTipo3 setValor(BigDecimal valor) {
+        this.valor = valor;
+        return this;
+    }
 
     @Override
     public String getTipoRegistro() {
         return TIPO_REGISTRO;
     }
 
-//    private String getCodigoOutrosValores() {
-//        return this.outrosValores.getTipo().getCodigo();
-//    }
-//
-//    private String getOutrosValoresValor() {
-//        return StringUtils.leftPad(this.outrosValores.getValor().setScale(2, RoundingMode.HALF_UP).toPlainString().replaceAll("[^0-9]", ""), 15, "0");
-//    }
-
     @Override
-    public String toLinha() {
-//        final StringBuilder linha = new StringBuilder();
-//        linha.append(TIPO_REGISTRO);
-//        linha.append(this.getCodigoOutrosValores());
-//        linha.append(this.getOutrosValoresValor());
-//        return linha.toString();
-        return null;
+    public NFSeBarueriRPSArquivoEnvioRegistroTipo3 fromLinha(String linha) {
+        return new NFSeBarueriRPSArquivoEnvioRegistroTipo3()
+                .setOutrosValoresTipo(NFSeBarueriOutrosValoresTipo.valueOfCodigo(linha.substring(1, 3)))
+                .setValor(new BigDecimal(StringUtils.trimToEmpty(linha.substring(3, 18))).movePointLeft(2));
     }
 
     @Override
-    public NFSeBarueriRPSArquivoEnvioRegistroTipo3 fromLinha(String linha) {
-        return null;
+    public String toLinha() {
+        return String.format("%s%s%s",
+                getTipoRegistro(),
+                this.outrosValoresTipo.getCodigo(),
+                StringUtils.leftPad(this.valor.setScale(2, RoundingMode.HALF_UP).toPlainString().replaceAll("[^0-9]", ""), 15, '0'));
     }
 }
